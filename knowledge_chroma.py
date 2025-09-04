6f90d2abcc5c9ed9192f2dc0f8fde6fb0ceea8f5
Ding <63493759+dingzhihuaSJTU@users.noreply.github.com>
2025-09-04 20:31:24 +0800

    Update knowledge_chroma.py

--- knowledge_chroma.py
+++ knowledge_chroma.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
 import re
